47abfa6cac3ad99c9e9f9c1bee460cdaa9c3b86e
csgrdcelia <casagrande.celia@gmail.com>
2019-12-29 21:00:16 +0100

    Remove check bits

--- encrypt.py
+++ encrypt.py
@@ -1,5 +1,12 @@
 
 
 def encrypt(file, key):
-    print(file)
-    print(key)
+    key = remove_check_bits(key)
+
+
+def remove_check_bits(key):
+    new_key = ""
+    for index, char in enumerate(key, start=1):
+        if index % 8 != 0:
+            new_key += char
+    return new_key
