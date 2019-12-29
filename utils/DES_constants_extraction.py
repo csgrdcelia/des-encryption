f4ec6d08ba7610c71af5fa90439e5582a31926dc
csgrdcelia <casagrande.celia@gmail.com>
2019-12-29 19:15:05 +0100

    Add zero matrix function & rename functions

--- utils/DES_constants_extraction.py
+++ utils/DES_constants_extraction.py
@@ -1,146 +1,150 @@
 # Récupère un tableau associatif avec les constantes du chiffrement DES
 def recupConstantesDES():
-    f = open("DES_constants.txt", "r")
-    txt = f.read()
-    f.close()
+    file = open("DES_constants.txt", "r")
+    txt = file.read()
+    file.close()
 
-    sdl = '\n'
+    line_break = '\n'
 
-    X = dict()
+    constant_dictionary = dict()
 
     # Permutation initiale
-    clef = "PI"
-    X[clef] = MatZero(1, 64)
-    deb = txt.find(clef + ' =')
-    fin = txt.find('FIN ' + clef)
-    col = 0
-    while txt[deb] != sdl and deb < fin:
-        deb += 1
-    deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 64
-        while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin: deb += 1
+    key = "PI"
+    constant_dictionary[key] = zero_matrix(1, 64)
+    start = txt.find(key + ' =')
+    end = txt.find('FIN ' + key)
+    column = 0
+    while txt[start] != line_break and start < end:
+        start += 1
+    start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 64
+        while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end: start += 1
 
-        col += 1
+        column += 1
 
         # Permutation initiale inverse
-        clef = "PI_I"
-        X[clef] = MatZero(1, 64)
-        deb = txt.find(clef + ' =')
-        fin = txt.find('FIN ' + clef)
-        col = 0
-        while txt[deb] != sdl and deb < fin:
-            deb += 1
-        deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 64
-        while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin: deb += 1
+        key = "PI_I"
+        constant_dictionary[key] = zero_matrix(1, 64)
+        start = txt.find(key + ' =')
+        end = txt.find('FIN ' + key)
+        column = 0
+        while txt[start] != line_break and start < end:
+            start += 1
+        start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 64
+        while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end: start += 1
 
-        col += 1
+        column += 1
 
     # Fonction d'expantion
-    clef = "E"
-    X[clef] = MatZero(1, 48)
-    deb = txt.find(clef + ' =')
-    fin = txt.find('FIN ' + clef)
-    col = 0
-    while txt[deb] != sdl and deb < fin: deb += 1
-    deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 48
-        while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin: deb += 1
+    key = "E"
+    constant_dictionary[key] = zero_matrix(1, 48)
+    start = txt.find(key + ' =')
+    end = txt.find('FIN ' + key)
+    column = 0
+    while txt[start] != line_break and start < end: start += 1
+    start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 48
+        while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end: start += 1
 
-        col += 1
+        column += 1
 
     # Permutation
-    clef = "PERM"
-    X[clef] = MatZero(1, 32)
-    deb = txt.find(clef + ' =')
-    fin = txt.find('FIN ' + clef)
-    col = 0
-    while txt[deb] != sdl and deb < fin: deb += 1
-    deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while (ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin):
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 32
-        while (not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin): deb += 1
+    key = "PERM"
+    constant_dictionary[key] = zero_matrix(1, 32)
+    start = txt.find(key + ' =')
+    end = txt.find('FIN ' + key)
+    column = 0
+    while txt[start] != line_break and start < end: start += 1
+    start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while (ord('0') <= ord(txt[start]) <= ord('9') and start < end):
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 32
+        while (not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end): start += 1
 
-        col += 1
+        column += 1
 
     # Première permutation des clefs
-    clef = "CP_1"
-    X[clef] = MatZero(1, 56)
-    deb = txt.find(clef + ' =')
-    fin = txt.find('FIN ' + clef)
-    col = 0
-    while txt[deb] != sdl and deb < fin: deb += 1
-    deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 56
-        while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin: deb += 1
+    key = "CP_1"
+    constant_dictionary[key] = zero_matrix(1, 56)
+    start = txt.find(key + ' =')
+    end = txt.find('FIN ' + key)
+    column = 0
+    while txt[start] != line_break and start < end: start += 1
+    start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 56
+        while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end: start += 1
 
-        col += 1
+        column += 1
 
     # Seconde permutation des clefs
-    clef = "CP_2"
-    X[clef] = MatZero(1, 48)
-    deb = txt.find(clef + ' =')
-    fin = txt.find('FIN ' + clef)
-    col = 0
-    while txt[deb] != sdl and deb < fin: deb += 1
-    deb += 1
-    while deb < fin:
-        X[clef][0][col] = 0
-        while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-            X[clef][0][col] = 10 * X[clef][0][col] + int(txt[deb])
-            deb += 1
-        X[clef][0][col] -= 1  # Car les entiers sont entre 1 et 48
-        while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin: deb += 1
+    key = "CP_2"
+    constant_dictionary[key] = zero_matrix(1, 48)
+    start = txt.find(key + ' =')
+    end = txt.find('FIN ' + key)
+    column = 0
+    while txt[start] != line_break and start < end: start += 1
+    start += 1
+    while start < end:
+        constant_dictionary[key][0][column] = 0
+        while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+            constant_dictionary[key][0][column] = 10 * constant_dictionary[key][0][column] + int(txt[start])
+            start += 1
+        constant_dictionary[key][0][column] -= 1  # Car les entiers sont entre 1 et 48
+        while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end: start += 1
 
-        col += 1
+        column += 1
 
     # Les 8 fonctions de substitution (numéroté de 0 à 7)
-    clef = "S"
-    X[clef] = dict()
+    key = "S"
+    constant_dictionary[key] = dict()
     for i in range(0, 8):
-        X[clef][i] = MatZero(4, 16)
-        deb = txt.find(clef + str(i + 1) + ' =')
-        fin = txt.find('FIN ' + clef + str(i + 1))
-        col = 0
-        lig = 0
-        while txt[deb] != sdl and deb < fin: deb += 1
-        deb += 1
-        while deb < fin:
-            X[clef][i][lig][col] = 0
-            while ord('0') <= ord(txt[deb]) <= ord('9') and deb < fin:
-                X[clef][i][lig][col] = 10 * X[clef][i][lig][col] + int(txt[deb])
-                deb += 1
+        constant_dictionary[key][i] = zero_matrix(4, 16)
+        start = txt.find(key + str(i + 1) + ' =')
+        end = txt.find('FIN ' + key + str(i + 1))
+        column = 0
+        line = 0
+        while txt[start] != line_break and start < end: start += 1
+        start += 1
+        while start < end:
+            constant_dictionary[key][i][line][column] = 0
+            while ord('0') <= ord(txt[start]) <= ord('9') and start < end:
+                constant_dictionary[key][i][line][column] = 10 * constant_dictionary[key][i][line][column] + int(txt[start])
+                start += 1
 
-            while not (ord('0') <= ord(txt[deb]) <= ord('9')) and deb < fin:
-                if txt[deb] == sdl and col >= 0:
-                    lig += 1
-                    col = -1
-                deb += 1
+            while not (ord('0') <= ord(txt[start]) <= ord('9')) and start < end:
+                if txt[start] == line_break and column >= 0:
+                    line += 1
+                    column = -1
+                start += 1
 
-            col += 1
+            column += 1
 
-    return X
+    return constant_dictionary
+
+
+def zero_matrix(x, y):
+    return [[0] * y for _ in range(x)]
