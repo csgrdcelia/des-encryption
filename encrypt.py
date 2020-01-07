820bd672c82aeec7d50c47131176810319854b1e
Vincent Guénin <guenin.vincent4@gmail.com>
2020-01-07 16:25:47 +0100

    Add cut-off

--- encrypt.py
+++ encrypt.py
@@ -4,10 +4,10 @@ from textwrap import wrap
 
 
 def encrypt(file, key):
-    key = permutation(key, des_constants["CP_1"][0], 1)
+    key = permutation(key, des_constants["CP_1"][0], True)
 
 
-def permutation(initial_key, permutation_key, skip_8=0):
+def permutation(initial_key, permutation_key, skip_8=False):
     new_key = []
 
     for permutation_index in permutation_key:
@@ -38,6 +38,11 @@ def initial_permutation(message_parts):
         index += 1
 
     return message_parts
+
+
+def cut_off(key):
+    middle = len(key) // 2
+    return key[:middle], key[middle:]
 
 
 des_constants = get_des_constants()
