c2fe8a6077cdddc9cd5df16c8ee4d360bf6961a1
csgrdcelia <casagrande.celia@gmail.com>
2020-01-08 18:01:33 +0100

    Fix inverse permutation (list length) and remove check bits

--- decrypt.py
+++ decrypt.py
@@ -4,12 +4,13 @@ from utils.des_constants_extraction import get_des_constants
 
 def decrypt(message, key):
     # TODO: rounds, etc
-    initial_key = inverse_permutation(get_encrypted_key_after_first_permutation(), des_constants["CP_1"][0], True)
+    initial_key_with_empty_check_bits = inverse_permutation(get_encrypted_key_after_first_permutation(), des_constants["CP_1"][0], True)
+    initial_key = remove_check_bits(initial_key_with_empty_check_bits)
     print(initial_key)
 
 
 def inverse_permutation(encrypted_key, permutation_key, skip_8):
-    list_length = len(permutation_key) + 7 if skip_8 else len(permutation_key) - 1
+    list_length = len(permutation_key) + 8
     initial_key = [0] * list_length
     for permutation_index in permutation_key:
         if skip_8:
@@ -21,6 +22,13 @@ def inverse_permutation(encrypted_key, permutation_key, skip_8):
 
 des_constants = get_des_constants()
 
+
+def remove_check_bits(key):
+    new_key = []
+    for index, number in enumerate(key, start=1):
+        if index % 8 != 0:
+            new_key.append(number)
+    return new_key
 
 def get_encrypted_key_after_first_permutation():
     key = "11000000000111110100100011110010111101001001011010111111"
